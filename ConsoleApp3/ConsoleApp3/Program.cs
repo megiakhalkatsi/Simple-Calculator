64bad861588b6da6a62d942d4160e74226821e81
Megi Akhalkatsi <megi.akhalkatsi@gmail.com>
2018-10-09 13:55:21 +0400

    დავამატე დავალების კოდი
    დავამატე მოცემული დავალების ფოლდერი, სადაც ნახავთ მარტივი კალკულატორის კოდს

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ამოცანა #1: მარტივი კალკულატორი switch-ით
+
+            Console.Write("sheiyvanet pirveli ricxvi   ");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("sheiyvanet matematikuri operatori ( +, -, *, / )   ");
+            string operation = Console.ReadLine();
+
+            Console.Write("sheiyvanet meore ricxvi   ");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            int result;
+
+
+            switch (operation)
+            {
+                case "+":
+                    result = numberOne + numberTwo;
+                    Console.WriteLine(numberOne + " " + operation + " " + numberTwo + " = " + result);
+                    break;
+                case "-":
+                    result = numberOne - numberTwo;
+                    Console.WriteLine(numberOne + " " + operation + " " + numberTwo + " = " + result);
+                    break;
+                case "*":
+                    result = numberOne * numberTwo;
+                    Console.WriteLine(numberOne + " " + operation + " " + numberTwo + " = " + result);
+                    break;
+                case "/":
+                    result = numberOne / numberTwo;
+                    Console.WriteLine(numberOne + " " + operation + " " + numberTwo + " = " + result);
+                    break;
+            }
+            
+            
+
+        }
+    }
+}
